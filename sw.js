646ce371553324154b7fcaadac2c46ed02313750
gilles-dasilva <65540200+gilles-dasilva@users.noreply.github.com>
2026-05-16 17:45:30 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Changer le numéro de version force la mise à jour pour tous
 // ============================================================
 
-const VERSION = '2026-v57';
+const VERSION = '2026-v58';
 const CACHE = 'japon-2026-' + VERSION;
 
 // À l'installation : cache uniquement le fichier principal
